d0e0ad75ae467e46d5055f6294b2878f99a0893f
Eduardo Zumbado Granados <42503164+Eduardo2108@users.noreply.github.com>
2020-06-03 20:27:08 -0600

    Added bubblesort

--- DataStructures_Algorithms_I/src/SortingAlgorithms/BubbleSort.java
+++ DataStructures_Algorithms_I/src/SortingAlgorithms/BubbleSort.java
@@ -1,4 +1,15 @@
 package SortingAlgorithms;
 
 public class BubbleSort {
+
+    public static void sort(DList lista) {
+        for (DNode i = lista.getHead(); i != null; i = i.getNext()) {
+            for (DNode j = lista.getHead(); j != null; j = j.getNext()) {
+                if (i.getData().compareTo(j.getData()) < 0) {
+                    lista.swap(i, j);
+                }
+            }
+        }
+    }
 }
+
